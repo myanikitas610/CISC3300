bd8f2b84f23415af88e231de6ea5c751a37865dc
Mya Nikitas <mnikitas@fordham.edu>
2024-02-13 17:34:38 -0500

    finished question 5

---
+++ homework4-q5.js
@@ -0,0 +1,67 @@
+const cats = [
+    {
+        name: 'Charlie',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lily',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Coco',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Oreo',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Luna',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Milo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lola',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Leo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Willow',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Bella',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Max',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Cleo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lucy',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Daisy',
+        adoptionStatus: 'available'
+    },
+]
+
+const adoptedCats = [];
+for(const cat of cats){
+    if (cat.adoptionStatus === 'available')
+    {
+        adoptedCats.push(cat.name);
+    }
+}
+console.log( "Your newly adopted cats are " + adoptedCats);
